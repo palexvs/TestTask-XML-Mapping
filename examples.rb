e4e45ac7248445416cd3c3bcab62b6dfc07be661
Alexey <palexvs@gmail.com>
2012-10-23 00:39:04 +0300

    initial commit

---
+++ examples.rb
@@ -0,0 +1,13 @@
+module Examples
+	
+	require './device'
+
+	resp = CustomerResponse.load_from_file("task.xml")
+	puts resp.inspect
+	puts resp.to_json
+	puts resp.to_hash
+
+	resp.device.location = "SomeLocation"
+	puts resp.to_hash
+	resp.save_to_xml.write $stdout,2
+end
